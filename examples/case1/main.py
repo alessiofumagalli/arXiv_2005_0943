b81d4a96e3d0deef85a826cda53b7d3e032da5ff
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-07-04 18:35:39 +0200

    update example1 according to the new implementation of the reaction class

--- examples/case1/main.py
+++ examples/case1/main.py
@@ -4,59 +4,41 @@ import porepy as pp
 import sys; sys.path.insert(0, "../../src/")
 from reaction import Reaction
 
-sys.path.insert(0, "../common/")
-from common import reaction_fct
+# reaction function, parameter: solute (u), precipitate (w), temperature (theta)
+def reaction_fct(u, w, theta, tol=1e-15):
+    r = np.power(u, 2)
+    return lmbda(theta) * ((w>tol)*np.maximum(1 - r, 0) - np.maximum(r - 1, 0))
 
-def set_data():
-    data = {}
-
-    # reference name of variable
-    data["solute"] = "solute"
-    data["precipitate"] = "precipitate"
-
-    # reference length
-    reference = {}
-    reference["length"] = 1
-    # reference Darcy velocity
-    reference["velocity"] = 1e-13
-    # reference concentration of solute at equilibrium
-    reference["gamma_eq"] = 0.16
-    # reference temperature (K)
-    reference["theta"] = 100+273 #pp.CELSIUS_to_KELVIN(100)
-    data["reference"] = reference
-
-    # reaction functions
-    data["reaction"] = reaction_fct
-
-    # initial datum
-    #gamma_0=@(x) 0*(x>L/3).*(x<2*L/3);
-    #C_0=@(x) 4*(x>L/3).*(x<2*L/3);
-
-    #Nx=100;
-
-    # time loop
-    data["num_steps"] = 5
-    data["end_time"] = 2
-    data["deltaT"] = 0.002 #data["end_time"]/data["num_steps"]
-
-    #xx=[1/Nx*0.5: 1/Nx: 1-0.5/Nx];
-    #dif=1.0e-13;
-
-    #Vm=0.0226;
-
-    return data
+# temperature dependent ?????????
+def lmbda(theta):
+    return 8.37e-6*np.exp(-6e4/8.314/theta)
 
 def main():
 
-    data = set_data()
-    r = Reaction(**data)
+    # time loop
+    num_steps = 5
+    end_time = 2
+    deltaT = end_time / float(num_steps)
+
+    tol_react = 1e-12
+
+    param = {
+        "length": 1, # reference length
+        "velocity": 1e-13, # reference Darcy velocity
+        "gamma_eq": 0.16, # reference concentration of solute at equilibrium
+        "theta": 100+273, # reference temperature (K)
+        "reaction": reaction_fct # reaction functions
+    }
+
+    r = Reaction(tol_react)
+    r.set_data(param)
 
     u = np.array([1, 0.2, 2])
     w = np.array([0.2, 0.01, 0.2])
     uw = np.stack((u, w))
 
-    for _ in np.arange(data["num_steps"]):
-        uw = r.step(uw, data["deltaT"])
+    for _ in np.arange(num_steps):
+        uw = r.step(uw, deltaT)
         print(uw)
 
 if __name__ == "__main__":
