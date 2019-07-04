290066425edc6d835472da037f765beab20c6a93
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-07-04 18:36:25 +0200

    now case2 is similar to the work of Agosti, Giovanardi, and Scotti. to make it as reference for the code

--- examples/case2/main.py
+++ examples/case2/main.py
@@ -3,10 +3,17 @@ import scipy.sparse as sps
 import porepy as pp
 
 import sys; sys.path.insert(0, "../../src/")
-from flow import Flow
 
-from concentration import Concentration
+from flow import Flow
+from transport import Transport
 from reaction import Reaction
+
+# ------------------------------------------------------------------------------#
+
+# reaction function, parameter: solute (u), precipitate (w), temperature (theta)
+def reaction_fct(u, w, theta, tol=1e-15):
+    r = np.power(u, 2)
+    return (w>tol)*np.maximum(1 - r, 0) - np.maximum(r - 1, 0)
 
 # ------------------------------------------------------------------------------#
 
@@ -25,8 +32,8 @@ def bc_flag_flow(g, data, tol):
     bc_val = np.zeros(g.num_faces)
 
     labels[in_flow + out_flow] = "dir"
-    bc_val[b_faces[in_flow]] = 0
-    bc_val[b_faces[out_flow]] = 10
+    bc_val[b_faces[in_flow]] = 0.5
+    bc_val[b_faces[out_flow]] = 0
 
     return labels, bc_val
 
@@ -43,14 +50,17 @@ def bc_flag_solute(g, data, tol):
     in_flow = b_face_centers[1] < 0 + tol
 
     # define the labels and values for the boundary faces
-    labels = np.array(["neu"] * b_faces.size)
+    labels_diff = np.array(["neu"] * b_faces.size)
+    labels_adv = np.array(["neu"] * b_faces.size)
+
+    labels_diff[in_flow] = "dir"
+    labels_adv[in_flow + out_flow] = "dir"
+
     bc_val = np.zeros(g.num_faces)
-
-    labels[in_flow + out_flow] = "dir"
     bc_val[b_faces[in_flow]] = 0
-    bc_val[b_faces[out_flow]] = 1
+    bc_val[b_faces[out_flow]] = 0
 
-    return labels, bc_val
+    return labels_diff, labels_adv, bc_val
 
 # ------------------------------------------------------------------------------#
 
@@ -69,12 +79,13 @@ def create_gb(file_name, mesh_size):
 
 def main():
     tol = 1e-6
+    tol_react = 1e-12
 
-    mesh_size = np.power(2., -2)
+    mesh_size = np.power(2., -6)
 
     end_time = 1
-    num_steps = 20
-    time_step = end_time / num_steps
+    num_steps = 100
+    time_step = end_time / float(num_steps)
 
     # the flow problem
     param_flow = {
@@ -92,6 +103,15 @@ def main():
         "mass_weight": 1.0/time_step,  # inverse of the time step
     }
 
+    # the reaction part
+    param_react = {
+        "length": 1,
+        "velocity": 1,
+        "gamma_eq": 1,
+        "theta": 0,
+        "reaction": reaction_fct,
+    }
+
     gb = create_gb("network.csv", mesh_size)
     gb.set_porepy_keywords()
 
@@ -100,7 +120,11 @@ def main():
     # declare the modles
     model_flow = "flow"
     model_solute = "solute"
-    model_precipitate = "precipitate"
+    model_reaction = "reaction"
+
+    # exporter
+    save = pp.Exporter(gb, "case2", folder="solution")
+    save_vars = ["pressure", "P0_darcy_flux", "solute", "precipitate"]
 
     discr_flow = Flow(gb, model_flow, tol)
 
@@ -115,33 +139,52 @@ def main():
 
     # post processing
     discr_flow.extract(x_flow)
-    discr_flow.export()
 
     # -- transport -- #
 
     # declare the solute and precipitate
-    discr_conc = Concentration(gb, [model_solute, model_precipitate], tol)
+    discr_solute = Transport(gb, model_solute, tol)
+    discr_react = Reaction(tol_react)
 
     # set the data
-    param_conc = {model_solute: param_solute, model_precipitate: param_solute}
-    bc_flag_conc = {model_solute: bc_flag_solute, model_precipitate: bc_flag_solute}
-    discr_conc.set_data(param_conc, bc_flag_conc)
+    discr_solute.set_data(param_solute, bc_flag_solute)
+    discr_react.set_data(param_react)
 
     # set the flux from the flow equation
-    discr_conc.set_flux(discr_flow.flux, discr_flow.mortar)
+    discr_solute.set_flux(discr_flow.flux, discr_flow.mortar)
 
     # time loop
-    x_conc = np.zeros(discr_conc.shape())
+    x_conc = np.ones((2, discr_solute.shape()))
+
+    for g, _ in gb:
+        if g.dim == 2:
+            x_conc[1, :] = 0.8*(np.logical_and(np.abs(g.cell_centers[0, :] - 0.5) < 0.1,
+                                               np.abs(g.cell_centers[1, :] - 0.5) < 0.1))
+
+    # post process
+    discr_solute.matrix_rhs() ##################################################
+    discr_solute.extract(x_conc[0, :], "solute")
+    discr_solute.extract(x_conc[1, :], "precipitate")
+
+    save.write_vtk(save_vars, time_step=0)
+
     for i in np.arange(num_steps):
 
+        # compute the matrices and rhs
+        A, M, b = discr_solute.matrix_rhs()
+
+        # solve the advective and diffusive step
+        x_conc[0, :] = sps.linalg.spsolve(M + A, M * x_conc[0, :] + b)
+
         # do one step
-        x_conc = discr_conc.one_step(x_conc, i)
+        x_conc = discr_react.step(x_conc, time_step)
 
         # post process
-        discr_conc.extract(x_conc)
-        discr_conc.export(i)
+        discr_solute.extract(x_conc[0, :], "solute")
+        discr_solute.extract(x_conc[1, :], "precipitate")
+        save.write_vtk(save_vars, time_step=time_step*(i+1))
 
-    discr_conc.export_pvd(np.arange(num_steps))
+    save.write_pvd(np.arange(num_steps+1)*time_step)
 
 # ------------------------------------------------------------------------------#
 
