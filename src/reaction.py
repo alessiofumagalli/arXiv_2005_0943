e577c2251f263c8d5ece4fe9a4ed51ff9a0ef9cf
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-07-04 18:34:49 +0200

    for the reaction step fix the algorithm and the input data

--- src/reaction.py
+++ src/reaction.py
@@ -1,19 +1,25 @@
 import numpy as np
 
 class Reaction(object):
-    def __init__(self, **kwargs):
-        # set the name of the solute and precipitate
-        self.solute = kwargs["solute"]
-        self.precipitate = kwargs["precipitate"]
 
+    # ------------------------------------------------------------------------------#
+
+    def __init__(self, tol=1e-16, max_it=1e2):
+        self.tol = tol
+        self.max_it = max_it
+
+    # ------------------------------------------------------------------------------#
+
+    def set_data(self, data):
         # dimensionless parameter
-        ref = kwargs["reference"]
-        self.adim = ref["length"]/ref["gamma_eq"]/ref["velocity"]
+        self.adim = data["length"]/data["gamma_eq"]/data["velocity"]
 
-        self.theta = ref["theta"]
-        self.reaction = kwargs["reaction"]
+        self.theta = data["theta"]
+        self.reaction = data["reaction"]
 
-    def step(self, uw_0, deltaT, tol=1e-12, max_it=1e2):
+    # ------------------------------------------------------------------------------#
+
+    def step(self, uw_0, deltaT):
         # solution at the end of the procedure
         uw_n = np.zeros(uw_0.shape)
 
@@ -42,34 +48,39 @@ class Reaction(object):
         uw_n[:, pos_1] = uw_0[:, pos_1] + deltaT * self.reaction_fct(uw_1[:, pos_1])
 
         # if during the second step something goes wrong we still need to work on it
-        neg_n = pos_1[np.where(uw_n[:, pos_1] < 0)[0]]
+        neg_n = pos_1[np.where(uw_n[1, pos_1] < 0)[0]]
 
         for i in neg_n:
             # initialization of the bisection algorithm
-            err, it = np.abs(uw_n[1, i]), 0
-            deltaT_a, deltaT_b = 0, deltaT
+            if uw_n[1, i] < 0:
+                err = np.abs(uw_n[1, i])
+                it = 0
+                deltaT_a, deltaT_b = 0, deltaT
 
-            while err > tol and it < max_it:
-                deltaT_mid = 0.5*(deltaT_a + deltaT_b)
-                # re-do the step
-                uw_bisec = uw_0[:, i, np.newaxis] + 0.5 * deltaT_mid * reaction_0[:, i, np.newaxis]
-                uw_bisec = uw_0[:, i, np.newaxis] + deltaT_mid * self.reaction_fct(uw_bisec)
+                while err > self.tol and it < self.max_it:
+                    deltaT_mid = 0.5*(deltaT_a + deltaT_b)
+                    # re-do the step
+                    uw_bisec = uw_0[:, i, np.newaxis] + 0.5 * deltaT_mid * reaction_0[:, i, np.newaxis]
+                    uw_bisec = uw_0[:, i, np.newaxis] + deltaT_mid * self.reaction_fct(uw_bisec)
 
-                if uw_bisec[1, :] > 0:
-                    deltaT_a = deltaT_mid
-                else:
-                    deltaT_b = deltaT_mid
+                    if uw_bisec[1, :] > 0:
+                        deltaT_a = deltaT_mid
+                    else:
+                        deltaT_b = deltaT_mid
 
-                it += 1
-                err = np.abs(uw_bisec[1, :])
+                    it += 1
+                    err = np.abs(uw_bisec[1, :])
 
-            # we are at convergence, we want to avoid negative numbers
-            uw_bisec[1, :] = 0
-            # then finish with the last part of the deltaT
-            uw_corrected = uw_bisec + 0.5 * (deltaT - deltaT_mid) * self.reaction_fct(uw_bisec)
-            uw_n[:, i, np.newaxis] = uw_bisec + (deltaT - deltaT_mid) * self.reaction_fct(uw_corrected)
+
+                # we are at convergence, we want to avoid negative numbers
+                uw_bisec[1, :] = 0
+                # then finish with the last part of the deltaT
+                uw_corrected = uw_bisec + 0.5 * (deltaT - deltaT_mid) * self.reaction_fct(uw_bisec)
+                uw_n[:, i, np.newaxis] = uw_bisec + (deltaT - deltaT_mid) * self.reaction_fct(uw_corrected)
 
         return uw_n
+
+    # ------------------------------------------------------------------------------#
 
     def reaction_fct(self, uw):
         # set the reaction rate, we suppose given as a function with
@@ -80,3 +91,5 @@ class Reaction(object):
 
         val = self.reaction(uw[0, :], uw[1, :], self.theta)
         return self.adim * np.einsum("ij,j->ij", sign, val)
+
+    # ------------------------------------------------------------------------------#
