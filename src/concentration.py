446d015dc7665041441bf89dd6cd7a09c0706e21
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-07-04 18:35:15 +0200

    the concentration class is not really needed now, but let's keep track on the modifications

--- src/concentration.py
+++ src/concentration.py
@@ -3,37 +3,47 @@ import scipy.sparse as sps
 import porepy as pp
 
 from transport import Transport
+from reaction import Reaction
 
 class Concentration(object):
 
-    def __init__(self, gb, models, tol):
+    # ------------------------------------------------------------------------------#
+
+    def __init__(self, gb, models_trans, model_react, tol):
 
         self.gb = gb
         self.tol = tol
 
-        models = np.atleast_1d(models)
-        self.discr = {model: Transport(self.gb, model, self.tol) for model in models}
+        # set up the transport models
+        self.models_trans = np.atleast_1d(models_trans)
+        self.discr_trans = {m: Transport(self.gb, m, self.tol) for m in self.models_trans}
+
+        # set up the reaction models
+        # we assume only one reaction model
+        self.model_react = model_react
+        self.discr_react = Reaction(self.tol)
 
     # ------------------------------------------------------------------------------#
 
     def set_data(self, data, bc_flag):
-        [discr.set_data(data[model], bc_flag[model]) for model, discr in self.discr.items()]
+        [d.set_data(data[m], bc_flag[m]) for m, d in self.discr_trans.items()]
+        self.discr_react.set_data(data[self.model_react])
 
     # ------------------------------------------------------------------------------#
 
     def set_flux(self, flux_name, mortar_name):
-        [discr.set_flux(flux_name, mortar_name) for _, discr in self.discr.items()]
+        [d.set_flux(flux_name, mortar_name) for _, d in self.discr_trans.items()]
 
     # ------------------------------------------------------------------------------#
 
     def shape(self):
-        return [len(self.discr), self.gb.num_cells() + 2*self.gb.num_mortar_cells()]
+        return [len(self.discr_trans), self.gb.num_cells() + 2*self.gb.num_mortar_cells()]
 
     # ------------------------------------------------------------------------------#
 
     def one_step(self, x, time):
         # do the advective and diffusive step for each specie separately
-        for pos, discr in enumerate(self.discr.values()):
+        for pos, discr in enumerate(self.discr_trans.values()):
 
             # compute the matrices and rhs
             A, M, b = discr.matrix_rhs()
@@ -42,22 +52,23 @@ class Concentration(object):
             x[pos, :] = sps.linalg.spsolve(M + A, M * x[pos, :] + b)
 
         # do the reactive step
+        #x = self.discr_react.step(x, time)
 
         return x
 
     # ------------------------------------------------------------------------------#
 
     def extract(self, x):
-        [discr.extract(x[pos, :]) for pos, discr in enumerate(self.discr.values())]
+        [d.extract(x[p, :]) for p, d in enumerate(self.discr_trans.values())]
 
     # ------------------------------------------------------------------------------#
 
     def export(self, time_step=None):
-        [discr.export(time_step) for discr in self.discr.values()]
+        [d.export(time_step) for d in self.discr_trans.values()]
 
     # ------------------------------------------------------------------------------#
 
     def export_pvd(self, steps):
-        [discr.export_pvd(steps) for discr in self.discr.values()]
+        [d.export_pvd(steps) for d in self.discr_trans.values()]
 
     # ------------------------------------------------------------------------------#
