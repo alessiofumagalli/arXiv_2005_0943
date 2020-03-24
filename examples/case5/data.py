ef8cda14a24fa2535963384b01e590fa562a4dd9
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2020-03-24 08:59:17 +0100

    add case 5

---
+++ examples/case5/data.py
@@ -0,0 +1,230 @@
+import numpy as np
+
+# ------------------------------------------------------------------------------#
+
+def get_param():
+    # data problem
+
+    tol = 1e-6
+    end_time = 10
+    num_steps = end_time * 20
+    time_step = end_time / float(num_steps)
+
+    return {
+        "tol": tol,
+
+        "time": {
+            "end_time": end_time,
+            "num_steps": num_steps,
+            "step": time_step
+        },
+
+        # porosity
+        "porosity": {
+            "eta": 1,
+            "initial": initial_porosity
+        },
+
+        # aperture
+        "aperture": {
+            "eta": 5,
+            "initial": initial_aperture
+        },
+
+        # flow
+        "flow": {
+            "tol": tol,
+            "k": 1,
+            "k_t": 1e6, "k_n": 1e6,
+            "bc": bc_flow,
+        },
+
+        # advection and diffusion of solute
+        "solute_advection_diffusion": {
+            "tol": tol,
+            "l": 1e-6,
+            "l_t": 1e-6, "l_n": 1e-6,
+            "mass_weight": 1.0,
+            "bc": bc_solute,
+            "initial_solute": initial_solute,
+            "initial_precipitate": initial_precipitate,
+        },
+
+        # reaction of solute and precipitate
+        "solute_precipitate_reaction": {
+            "tol": tol,
+            "length": 1,
+            "velocity": 1,
+            "gamma_eq": 1,
+            "theta": 0,
+            "reaction": reaction_fct,
+            "tol_reaction": 1e-12,
+            "max_iter": 1e2,
+        },
+
+        # temperature
+        "temperature": {
+            "initial": initial_temperature
+        },
+
+    }
+
+# ------------------------------------------------------------------------------#
+
+# reaction function, parameter: solute (u), precipitate (w), temperature (theta)
+def reaction_fct(u, w, theta, tol=1e-15):
+    l = 175*np.exp(-4/theta)
+    r = np.power(u, 2)
+    return l*((w>tol)*np.maximum(1 - r, 0) - np.maximum(r - 1, 0))
+
+# ------------------------------------------------------------------------------#
+
+def bc_flow(g, data, tol):
+    b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+    b_face_centers = g.face_centers[:, b_faces]
+
+    # define outflow type boundary conditions
+    out_flow = b_face_centers[1] > 1 - tol
+
+    # define inflow type boundary conditions
+    in_flow = b_face_centers[1] < 0 + tol
+
+    # define the labels and values for the boundary faces
+    labels = np.array(["neu"] * b_faces.size)
+    bc_val = np.zeros(g.num_faces)
+
+    labels[in_flow] = "dir"
+    labels[out_flow] = "dir"
+    bc_val[b_faces[in_flow]] = 1
+    bc_val[b_faces[out_flow]] = 0
+
+    return labels, bc_val
+
+# ------------------------------------------------------------------------------#
+
+def bc_solute(g, data, tol):
+    b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+    b_face_centers = g.face_centers[:, b_faces]
+
+    # define outflow type boundary conditions
+    out_flow = b_face_centers[1] > 1 - tol
+
+    # define inflow type boundary conditions
+    in_flow = b_face_centers[1] < 0 + tol
+
+    # define the labels and values for the boundary faces
+    labels_diff = np.array(["neu"] * b_faces.size)
+    labels_adv = np.array(["neu"] * b_faces.size)
+
+    labels_diff[in_flow] = "dir"
+    labels_adv[in_flow] = "dir"
+    labels_adv[out_flow] = "dir"
+
+    bc_val = np.zeros(g.num_faces)
+    bc_val[b_faces[in_flow]] = 1.1
+    bc_val[b_faces[out_flow]] = 0
+
+    return labels_diff, labels_adv, bc_val
+
+# ------------------------------------------------------------------------------#
+
+def initial_solute(g, data, tol):
+    solute = np.zeros(g.num_cells)
+    return solute
+
+# ------------------------------------------------------------------------------#
+
+def initial_precipitate(g, data, tol):
+    if is_flag(g):
+        precipitate = 0.5*np.ones(g.num_cells)
+    else:
+        precipitate = np.zeros(g.num_cells)
+    return precipitate
+
+# ------------------------------------------------------------------------------#
+
+def initial_porosity(g, data, tol):
+    if g.dim == 2: # NOTE: we are in 2d as dim_max
+        return 0.2
+    else:
+        # we set a zero porosity, meaning it is not active anymore
+        # the temporal scheme considered keeps this variable null
+        return np.zeros(g.num_cells)
+
+# ------------------------------------------------------------------------------#
+
+def initial_aperture(g, data, tol):
+    if g.dim == 2: # NOTE: we are in 2d as dim_max
+        # we set a zero aperture, meaning it is not active
+        # the temporal scheme considered keeps this variable null
+        aperture = np.zeros(g.num_cells)
+    else:
+        if is_flag(g):
+            aperture = 1e-3*np.ones(g.num_cells)
+        else:
+            aperture = 1e-2*np.ones(g.num_cells)
+    return aperture
+
+# ------------------------------------------------------------------------------#
+
+def initial_temperature(g, data, tol):
+    return g.cell_centers[1]
+
+# ------------------------------------------------------------------------------#
+
+def is_flag(g):
+    tol = 1e-3
+    # set the key for the low peremable fractures
+    if g.dim == 1:
+
+#        f_0 = (g.nodes[0, :] - 0.05)/(0.2200 - 0.05) - (g.nodes[1, :] - 0.4160)/(0.0624 - 0.4160)
+#        if np.sum(np.abs(f_0)) < tol:
+#            d["frac_num"] = 0
+#            d["k_t"] = 1e4
+#
+#        f_1 = (g.nodes[0, :] - 0.05)/(0.2500 - 0.05) - (g.nodes[1, :] - 0.2750)/(0.1350 - 0.2750)
+#        if np.sum(np.abs(f_1)) < tol:
+#            d["frac_num"] = 1
+#            d["k_t"] = 1e4
+#
+#        f_2 = (g.nodes[0, :] - 0.15)/(0.4500 - 0.15) - (g.nodes[1, :] - 0.6300)/(0.0900 - 0.6300)
+#        if np.sum(np.abs(f_2)) < tol:
+#            d["frac_num"] = 2
+#            d["k_t"] = 1e4
+
+        f_3 = (g.nodes[0, :] - 0.15)/(0.4 - 0.15) - (g.nodes[1, :] - 0.9167)/(0.5 - 0.9167)
+        if np.sum(np.abs(f_3)) < tol:
+            return True
+
+        f_4 = (g.nodes[0, :] - 0.65)/(0.849723 - 0.65) - (g.nodes[1, :] - 0.8333)/(0.167625 - 0.8333)
+        if np.sum(np.abs(f_4)) < tol:
+            return True
+
+#        f_5 = (g.nodes[0, :] - 0.70)/(0.849723 - 0.70) - (g.nodes[1, :] - 0.2350)/(0.167625 - 0.2350)
+#        if np.sum(np.abs(f_5)) < tol:
+#            d["frac_num"] = 5
+#            d["k_t"] = 1e4
+#
+#        f_6 = (g.nodes[0, :] - 0.60)/(0.8500 - 0.60) - (g.nodes[1, :] - 0.3800)/(0.2675 - 0.3800)
+#        if np.sum(np.abs(f_6)) < tol:
+#            d["frac_num"] = 6
+#            d["k_t"] = 1e4
+#
+#        f_7 = (g.nodes[0, :] - 0.35)/(0.8000 - 0.35) - (g.nodes[1, :] - 0.9714)/(0.7143 - 0.9714)
+#        if np.sum(np.abs(f_7)) < tol:
+#            d["frac_num"] = 7
+#            d["k_t"] = 1e4
+#
+#        f_8 = (g.nodes[0, :] - 0.75)/(0.9500 - 0.75) - (g.nodes[1, :] - 0.9574)/(0.8155 - 0.9574)
+#        if np.sum(np.abs(f_8)) < tol:
+#            d["frac_num"] = 8
+#            d["k_t"] = 1e4
+#
+#        f_9 = (g.nodes[0, :] - 0.15)/(0.4000 - 0.15) - (g.nodes[1, :] - 0.8363)/(0.9727 - 0.8363)
+#        if np.sum(np.abs(f_9)) < tol:
+#            d["frac_num"] = 9
+#            d["k_t"] = 1e4
+
+    return False
+
+# ------------------------------------------------------------------------------#
---
+++ examples/case5/main.py
@@ -0,0 +1,93 @@
+import numpy as np
+import scipy.sparse as sps
+import porepy as pp
+
+import sys; sys.path.insert(0, "../../src/")
+from scheme import Scheme
+
+from data import get_param
+
+# ------------------------------------------------------------------------------#
+
+def create_gb(file_name, mesh_size):
+    domain = {"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1}
+    network = pp.fracture_importer.network_2d_from_csv(file_name, domain=domain)
+
+    # assign the flag for the low permeable fractures
+    mesh_kwargs = {"mesh_size_frac": mesh_size, "mesh_size_min": mesh_size / 20}
+    gb = network.mesh(mesh_kwargs)
+
+    for _, d in gb:
+        d[pp.PRIMARY_VARIABLES] = {}
+        d[pp.DISCRETIZATION] = {}
+        d[pp.DISCRETIZATION_MATRICES] = {}
+
+    for _, d in gb.edges():
+        d[pp.PRIMARY_VARIABLES] = {}
+        d[pp.COUPLING_DISCRETIZATION] = {}
+        d[pp.DISCRETIZATION_MATRICES] = {}
+
+    return gb
+
+# ------------------------------------------------------------------------------#
+
+def main():
+
+    mesh_size = np.power(2., -5)
+    gb = create_gb("network_split.csv", mesh_size)
+
+    param = get_param()
+
+    scheme = Scheme(gb)
+    scheme.set_data(param)
+
+    # exporter
+    save = pp.Exporter(gb, "case5", folder_name="solution")
+    vars_to_save = scheme.vars_to_save()
+
+    # post process
+    scheme.extract()
+    save.write_vtk(vars_to_save, time_step=0)
+
+    for i in np.arange(param["time"]["num_steps"]):
+
+        print("processing", i, "step")
+
+        # -- DO THE FLOW PART -- #
+
+        # update the data from the previous time step
+        scheme.update_flow()
+        # solve the flow part
+        scheme.solve_flow()
+
+        # -- DO THE TRANSPORT PART -- #
+
+        # update the data from the previous time step
+        scheme.update_solute_precipitate()
+        # solve the advection and diffusion part
+        scheme.solve_solute_precipitate_advection_diffusion()
+        ## solve the reaction part
+        scheme.solve_solute_precipitate_rection()
+
+        ## -- DO THE POROSITY PART -- #
+
+        ## solve the porosity part
+        scheme.solve_porosity()
+
+        ## solve the aperture part
+        scheme.solve_aperture()
+
+        # post process
+        scheme.extract()
+        time = param["time"]["step"]*(i+1)
+        save.write_vtk(vars_to_save, time_step=time)
+
+    time = np.arange(param["time"]["num_steps"]+1)*param["time"]["step"]
+    save.write_pvd(time)
+
+# ------------------------------------------------------------------------------#
+
+if __name__ == "__main__":
+    main()
+
+# ------------------------------------------------------------------------------#
